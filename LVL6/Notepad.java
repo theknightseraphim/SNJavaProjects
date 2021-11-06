acd56447f8d9ea90852cdd3a14876cec5ceb0a04
Sergey Nikitin <theknightseraphim@gmail.com>
2021-11-06 23:39:06 +0300

    Create Notepad.java

---
+++ LVL6/Notepad.java
@@ -0,0 +1,15 @@
+package SNJavaProjects.LVL6;
+/*
+Блокнот для новых идей
+*/
+public class Notepad {
+    public static void main(String[] args) {
+        printIdea(new Idea());
+    }
+
+    public static class Idea(){
+        public String getDescription(){
+            return printIdea;
+        }
+    }
+}
