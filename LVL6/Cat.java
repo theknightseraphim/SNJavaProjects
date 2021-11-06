f559ee769cad68fc1ae730040f42fc7f36d7485a
Sergey Nikitin <theknightseraphim@gmail.com>
2021-11-06 17:15:09 +0300

    Create Cat.java

---
+++ LVL6/Cat.java
@@ -0,0 +1,21 @@
+package SNJavaProjects.LVL6;
+import java.util.ArrayList;
+
+/*
+Статические коты
+*/
+public class Cat {
+    //напишите тут ваш код
+
+    public Cat() {
+    }
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        printCats();
+    }
+
+    public static void printCats() {
+        //напишите тут ваш код
+    }
+}
