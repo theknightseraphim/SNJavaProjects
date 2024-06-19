efe5b2ccf3bd8ebd3990be65d57a2e377e81cff6
Sergey Nikitin <89871557+theknightseraphim@users.noreply.github.com>
2024-06-19 17:23:05 +0300

    Update Exercise_1.7_while.java
    Пользователь с клавиатуры вводит действительные числа до 0. Необходимо вычислить их среднее арифметическое. Гарантируется, что он введёт хотя бы одно ненулевое число.

--- Java_again/Exercise_1.7_while.java
+++ Java_again/Exercise_1.7_while.java
@@ -1,19 +1,21 @@
 import java.util.Scanner;
 
-class MyProgram{
-    public static void main(String[] args){
+/* Пользователь с клавиатуры вводит действительные числа до 0. Необходимо вычислить их среднее арифметическое.
+ * Гарантируется, что он введёт хотя бы одно ненулевое число.*/
+
+class MyProgram {
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
         double sum = 0;
         int count = 0;
 
-        while (number!=0)
-        {
+        while (number != 0) {
             count++;
             sum += number;
             number = scan.nextInt();
 
         }
-        System.out.println(sum/count);
+        System.out.println(sum / count);
     }
 }
