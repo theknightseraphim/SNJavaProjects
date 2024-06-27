7e3d2655a3bb22784a63944182aaef45ae59cc27
Sergey Nikitin <89871557+theknightseraphim@users.noreply.github.com>
2024-06-27 06:49:03 +0300

    Add files via upload

---
+++ Java_again/Exercise_1.9.1_for.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+/*Вводится 2 целых числа a и b (a <= b). Необходимо вычислить сумму всех чисел от а до b включительно.*/
+
+class MyProgram17{
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int num1 = scan.nextInt();
+        int num2 = scan.nextInt();
+        int sum = 0;
+
+        for (int i = num1; num1 <= num2; num1++){
+            sum += num1;
+            //System.out.println("summa chisel " + sum);
+        }
+        System.out.println(sum);
+        scan.close();
+    }
+}
