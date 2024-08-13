af9d77ee7c8168e5e6b935863eadbaf84a9d35c4
Sergey Nikitin <89871557+theknightseraphim@users.noreply.github.com>
2024-08-13 07:19:51 +0300

    Add files via upload

---
+++ Other_tasks/ASTON_QA_homework_1.java
@@ -0,0 +1,18 @@
+package SNJavaProjects.TestirovaniePO_Tasks.TestirovaniePO_Tasks;
+import java.util.Scanner;
+
+/*
+Составить алгоритм: если введенное число больше 7, то вывести "Привет"
+*/
+public class ASTON_QA_homework_1 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int number = scan.nextInt();
+
+        if (number > 7) {
+            System.out.println("Привет");
+        } else System.out.println("Need more numbers!");
+        scan.close();
+    }
+}
---
+++ Other_tasks/Lesson_3_Task4.java
@@ -0,0 +1,15 @@
+package SNJavaProjects.TestirovaniePO_Tasks;
+import java.io.IOException;
+
+
+public class Lesson_3_Task4 {
+    double pi = 3.14;
+    int r = 5;
+    double ploshadKruga = pi * (r*2);
+    int ploshadKvadrata = r * r;
+
+    public void print(){
+        System.out.println(ploshadKruga);
+        System.out.println(ploshadKvadrata);
+    }
+}
