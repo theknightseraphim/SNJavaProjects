b128deab0f7bb9437d1d907e4230e86710f6e2a3
Sergey Nikitin <89871557+theknightseraphim@users.noreply.github.com>
2024-08-13 07:20:18 +0300

    Add files via upload

---
+++ JavaRushTask/LVL8_Collections/KvartetMethods.java
@@ -0,0 +1,29 @@
+package SNJavaProjects.LVL8_Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Date;
+/*
+Квартет «Методы»
+*/
+public class KvartetMethods {
+    public static List getListForGet() {
+        return new ArrayList();
+    }
+
+    public static List getListForSet() {
+        return new ArrayList();
+    }
+
+    public static List getListForAddOrInsert() {
+        return new LinkedList();
+    }
+
+    public static List getListForRemove() {
+        return new LinkedList();
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
